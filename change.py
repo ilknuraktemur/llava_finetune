80a3de9ad7a00f5191c2c2e19cdcdcc21884b494
İlknur Aktemur <58196777+ilknuraktemur@users.noreply.github.com>
2023-11-20 16:30:35 +0300

    Update change.py

--- change.py
+++ change.py
@@ -10,5 +10,5 @@ for item in data:
     # change path
     item['image'] = item['image'].replace("/content/drive/MyDrive/sample_data_llava", "/workspace/ilknur/sample_data/data/sample_data_llava")
 # Save changes
-with open('/ workspace/ilknur/sample_data/data2.json', 'w') as file:
+with open('/workspace/ilknur/sample_data/data2.json', 'w') as file:
     json.dump(data, file, indent=2)
